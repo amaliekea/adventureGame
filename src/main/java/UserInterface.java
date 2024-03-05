84ad07963ce8b0badf375e668ba75bfec91fc939
HugTheMainframe <rasm65n4@stud.kea.dk>
2024-03-05 14:25:30 +0100

    changed switch til if-else (userInput)

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -9,12 +9,14 @@ public class UserInterface {
     public void startGame(){
 
         //Opsætter et do-while loop, så vi kan bevæge os rundt i rummene (se movePlayer metode i Adventure klassen)
+
         String helpinfo = "Enter north, east, south or west to navigate" +
                 "\nEnter \"look\" to get room information" +
                 "\nEnter \"exit\" to quit the game";
         String wayBlocked = "Can't go this way...";
         String exit = "Exiting game...";
         String invalid = "Invalid user input. Please enter north, east, south or west";
+
         String userInput = "";
         System.out.println(adventure.roomNameAndDescription());
 
