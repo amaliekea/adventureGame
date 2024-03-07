2d2f2cd4bc19c76cdfd96c14849ed15dd252a4c1
amaliekea <amje0001@stud.kea.dk>
2024-03-07 11:13:06 +0100

    first commit

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    //hej dette er amalies kode
-    public static void main(String[] args) {
-        UserInterface ui = new UserInterface(); //opretter ny ui
-        ui.menu(); //menu metode-kald
-
-    }
-}
